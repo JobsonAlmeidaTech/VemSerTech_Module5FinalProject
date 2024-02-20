716cdfe4fb85d8929818378373bfde776d9dc11d
Jobson Almeida <jobsonbox@gmail.com>
2024-02-20 16:06:38 -0300

    Changed method from put to patch on the route to update course (/auth/updateCourse)

--- routes/updateCourse.js
+++ routes/updateCourse.js
@@ -3,7 +3,7 @@ const routeUpdateCourse= express.Router()
 const checkIsAdmin = require("../controllers/checkIsAdmin")
 const updateCourse= require("../controllers/updateCourse")
 
-routeUpdateCourse.put("/auth/updateCourse", checkIsAdmin, async (req, res)=>{
+routeUpdateCourse.patch("/auth/updateCourse", checkIsAdmin, async (req, res)=>{
 
     updateCourse(req, res)
 
